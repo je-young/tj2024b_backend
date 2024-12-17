ad5698febff726fdb8f9648bc0412662b2877efc
je-young <jey2965@gmail.com>
2024-12-17 09:32:00 +0900

    클래스정의 / 객체생성

---
+++ tj2024b_backend/src/day06/Example3.java
@@ -0,0 +1,25 @@
+package day06;
+
+public class Example3 { // class s
+	
+
+	
+
+	public static void main(String[] args) { // main s
+		
+		// 1분복습 : 1.class 2.Person 3.int
+		// 생각해보가 : 샐활속의 객체들을 클래스 코드로 표현하기
+			// - 학생, 사람, 자동차, 주문 제외한다.
+			// - 작성후 카톡방에 제출
+		
+	} // main end
+
+} // class end
+
+public class Computer {
+	String 제조사;
+	String 제품시리얼넘버;
+	String 제품컬러;
+	String CPU사양;
+	String 메모리사양;
+}
