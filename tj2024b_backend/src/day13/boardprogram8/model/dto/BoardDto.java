3834a4d56034411fa041c9ae939a0dbcc63af12d
je-young <jey2965@gmail.com>
2024-12-26 12:36:52 +0900

    JAVA / MVC 실습파일생성

---
+++ tj2024b_backend/src/day13/boardprogram8/model/dto/BoardDto.java
@@ -0,0 +1,5 @@
+package day13.boardprogram8.model.dto;
+
+public class BoardDto {
+
+}
---
+++ tj2024b_backend/src/day13/boardprogram8/view/BoardView.java
@@ -0,0 +1,8 @@
+package day13.boardprogram8.view;
+
+public class BoardView { // class s
+  
+  // + 싱글톤
+  
+
+  } // class ene
